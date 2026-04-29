182869d4756e6db418dba71c4413d3ccbd7b5a6b
Ricardo031 <rjtb31@gmail.com>
2026-04-28 21:45:37 -0500

    first commit

---
+++ semana1/systemInteractive.js
@@ -0,0 +1,33 @@
+const prompt = require("prompt-sync")(); // para poder escribir en terminal
+
+let name = prompt("Enter your Name: ")
+let years = parseInt(prompt("Enter your year: "))
+const gender = "Male"
+let pretty = true
+
+//detect if years is a number
+while (isNaN(years)) {
+
+    console.log("Error: please Enter a valid year");
+    console.log("---------------------------");
+    years = parseInt(prompt("Enter a valid year: "))
+    
+}
+
+if(years < 18) {
+    console.log("-------------OK--------------");
+    console.log(`Hello ${name}, you're a minor. keep learning and enjoy the coddi`);
+    console.log(name, years, gender, pretty);
+} else {
+    console.log("-------------OK--------------");
+    console.log(`Hello ${name}, you're of legal age. Get ready for great opportunities in the world of programming!`);
+    console.log(name, years, gender, pretty);
+}
+
+
+
+
+
+
+
+
