8805285beef97f599cf772634ae00f6fe30701ea
Ricardo031 <rjtb31@gmail.com>
2026-05-04 15:14:44 -0500

    semana 2: task 1

---
+++ semana2/systemInteractive.js
@@ -0,0 +1,17 @@
+const prompt = require("prompt-sync")(); // para poder escribir en terminal
+
+// 1. Creación del objeto de productos
+const productos = [
+    { id: 1, nombre: "Laptop", precio: 1200 },
+    { id: 2, nombre: "Mouse", precio: 25 },
+    { id: 3, nombre: "Teclado", precio: 45 }
+];
+
+
+
+
+
+
+
+
+
