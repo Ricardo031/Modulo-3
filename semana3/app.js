770c9780ef4528bcfade7d3e425484593e6b135a
Ricardo031 <rjtb31@gmail.com>
2026-05-19 19:45:27 -0500

    Semana 3: task 2

---
+++ semana3/app.js
@@ -0,0 +1,8 @@
+let idea = document.getElementById('idea');
+let btn = document.getElementById('btnAgregar');
+let listaNotas = document.getElementById('listaNotas');
+
+console.log(idea);
+console.log(btn);
+console.log(listaNotas);
+
