bdb9997022b4feb3625d7bf35b7ee217385aad84
Ricardo031 <rjtb31@gmail.com>
2026-05-20 14:48:48 -0500

    Semana 3: leccion 5 y 6

--- semana3/app.js
+++ semana3/app.js
@@ -2,35 +2,68 @@ let idea = document.getElementById('idea');
 let btn = document.getElementById('btnAgregar');
 let listaNotas = document.getElementById('listaNotas');
 
+function obtenerIdeas() {
+    return JSON.parse(localStorage.getItem('ideas')) || [];
+}
+
+function guardarIdeas(ideas) {
+    localStorage.setItem('ideas', JSON.stringify(ideas));
+}
+
+console.log(obtenerIdeas());
+
+
 //%todo task 3. Agregar notas al DOM:
 btn.addEventListener('click', function (event) {
     event.preventDefault();
-    console.log(idea.value.trim());
 
-    if (idea.value.trim() === '') {
+    const textoIdea = idea.value.trim();
+
+    if (textoIdea === '') {
         alert('Por favor, escribe una idea.');
         return;
-    } else {
-        // Create new list item
-        listaNotas.style.display = 'flex';
+    }else{
+        const ideasGuardadas = obtenerIdeas();
+        ideasGuardadas.push(textoIdea);
+        guardarIdeas(ideasGuardadas);
+        mostrarNotas();
+        idea.value = '';
+    }
+
+    
+});
+
+function mostrarNotas() {
+    let valores = obtenerIdeas();
+    listaNotas.innerHTML = '';
+    if (valores.length === 0) {
+        listaNotas.style.display = 'none';
+        return;
+    }
+
+        listaNotas.style.display = 'flex'; // Mostrar la lista de notas si hay ideas guardadas. cuando hay mas de 0 ideas guardada.
+        valores.forEach(function (valor, indice) {
         let li = document.createElement('li');
         li.classList.add('idea-item');
         li.innerHTML = `<div class="idea-content">
-        <p>${idea.value}</p>
+        <p>${valor}</p>
         <button class="btn-eliminar">Eliminar</button>
-        </div>
-        ` //utilice el innerHTML para crear el contenido del elemento li, aunque me hayan pedido.textContent preferi usar el otro para mejoras visuales
-        console.log(li); //? Imprime en consola que se agregó la nota.
+        </div>`;
 
-        //%todo task 4. Eliminar notas del DOM:
         let btnEliminar = li.querySelector('.btn-eliminar');
         btnEliminar.addEventListener('click', function () {
-            listaNotas.removeChild(li);
-            console.log(`la lista quedo asi: ${listaNotas.innerHTML}`);
+            const ideasActualizadas = valores.filter(function (_, i) {
+                return i !== indice;
+            });
+
+            guardarIdeas(ideasActualizadas);
+            mostrarNotas();
         });
+
         listaNotas.appendChild(li);
-        
-        
-        idea.value = '';
-    }
-})
+    });
+}
+
+mostrarNotas() // Mostrar notas al cargar la página. esto es indispensable para que se muestren las ideas guardadas.
+
+
